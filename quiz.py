08071e4da98f66694993fccd9678d9b13933fda4
hjpark564 <phj050428@gmail.com>
2025-05-15 15:36:39 +0900

    1

--- quiz.py
+++ quiz.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.title("Quiz App")
 
-st.write("이 앱은 간단한 퀴즈를 제공합C니다. 아래 질문에 답해주세요!")
+st.write("이 앱은 간단한 퀴즈를 제공합니다. 아래 질문에 답해주세요!")
 
 
 question = "다음 중 박현준이 담당하는 세션은?"
@@ -16,3 +16,6 @@ if st.button("제출"):
         st.success("정답입니다!")
     else:
         st.error("오답입니다. 다시 시도해보세요.")
+
+
+
